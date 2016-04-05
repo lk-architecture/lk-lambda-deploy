9e14b16c8a3fe4743d63ffd14575ae9dbdd77546
Paolo Scanferla <paolo.scanferla@mondora.com>
2016-04-05 10:20:55 +0200

    Use process.env.HOME instead of ~

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const LOG_LEVEL = process.env.LOG_LEVEL || "info";
-export const BUNDLES_DIRECTORY = process.env.BUNDLES_DIRECTORY || "~/lk-deploy/bundles";
-export const SOURCES_DIRECTORY = process.env.SOURCES_DIRECTORY || "~/lk-deploy/sources";
+export const BUNDLES_DIRECTORY = (
+    process.env.BUNDLES_DIRECTORY || `${process.env.HOME}/lk-deploy/bundles`
+);
+export const SOURCES_DIRECTORY = (
+    process.env.SOURCES_DIRECTORY || `${process.env.HOME}/lk-deploy/sources`
+);
--- src/steps/0.setup.js
+++ src/steps/0.setup.js
@@ -1,10 +1,12 @@
-import {emptyDirSync} from "fs-extra";
+import {emptyDirSync, mkdirpSync} from "fs-extra";
 import log from "../services/logger";
 
 export default function setup ({bundleDir, sourceDir}) {
     // Create the necessary directories
     log.info(`Creating directory ${bundleDir}`);
+    mkdirpSync(bundleDir);
     emptyDirSync(bundleDir);
     log.info(`Creating directory ${sourceDir}`);
+    mkdirpSync(sourceDir);
     emptyDirSync(sourceDir);
 }
