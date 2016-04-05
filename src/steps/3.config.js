6288595ec0bcaa6cb4e2cc2864607e7e941e1163
Manuel Serra <nitroin@hotmail.com>
2016-04-05 17:20:28 +0200

    Fixed error in config step

--- src/steps/3.config.js
+++ src/steps/3.config.js
@@ -1,4 +1,4 @@
-import {writeFileSync} from "fs-extra";
+import {mkdirpSync, writeFileSync} from "fs-extra";
 import execSync from "../services/exec-sync";
 import log from "../services/logger";
 
@@ -8,9 +8,10 @@ export default function config ({sourceDir, environmentVariables}) {
     // module
     const dotEnv = environmentVariables.map(({key, value}) => `${key}=${value}\n`);
     log.info("Writing environment variables");
+    mkdirpSync(`${sourceDir}/tmp/bundle/`);
     execSync(`unzip -q ${sourceDir}/bundle.zip -d ${sourceDir}/tmp/bundle/`);
     writeFileSync(`${sourceDir}/tmp/bundle/.env`, dotEnv, "utf8");
     execSync(`rm ${sourceDir}/bundle.zip`);
-    execSync(`zip -qr bundle.zip ${sourceDir}/tmp/bundle/`);
+    execSync(`zip -qr ${sourceDir}/bundle.zip ${sourceDir}/tmp/bundle/`);
     execSync(`rm -r ${sourceDir}/tmp`);
 }
