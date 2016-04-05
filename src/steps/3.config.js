d0746b2bfc03ea0a9aeef9a4d5776d1577267c5c
Manuel Serra <nitroin@hotmail.com>
2016-04-05 16:07:51 +0200

    Added .env file to zipped bundle

--- src/steps/3.config.js
+++ src/steps/3.config.js
@@ -1,11 +1,16 @@
 import {writeFileSync} from "fs-extra";
+import execSync from "../services/exec-sync";
 import log from "../services/logger";
+
 
 export default function config ({sourceDir, environmentVariables}) {
     // Write environment variables in the format required by the `dotenv` npm
     // module
     const dotEnv = environmentVariables.map(({key, value}) => `${key}=${value}\n`);
     log.info("Writing environment variables");
-    // TODO inject into bundle.zip
-    writeFileSync(`${sourceDir}/.env`, dotEnv, "utf8");
+    execSync(`unzip -q ${sourceDir}/bundle.zip -d ${sourceDir}/tmp/bundle/`);
+    writeFileSync(`${sourceDir}/tmp/bundle/.env`, dotEnv, "utf8");
+    execSync(`rm ${sourceDir}/bundle.zip`);
+    execSync(`zip -qr bundle.zip ${sourceDir}/tmp/bundle/`);
+    execSync(`rm -r ${sourceDir}/tmp`);
 }
